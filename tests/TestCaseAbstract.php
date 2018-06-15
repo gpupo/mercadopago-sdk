a02836d992aa4078f4056057bf100d496597b4ad
Alison Valente <alisonvm@gmail.com>
2018-06-15 11:52:43 -0300

    Add translator and test

--- tests/TestCaseAbstract.php
+++ tests/TestCaseAbstract.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  *
  */
 
-namespace Gpupo\Tests\MercadopagoSdk;
+namespace Gpupo\MercadopagoSdk\Tests;
 
 use Gpupo\MercadopagoSdk\Factory;
 use Gpupo\Tests\CommonSdk\TestCaseAbstract as CommonSdkTestCaseAbstract;
